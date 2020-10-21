ead6ee25c83f4db6b4b5488ba41f3defc0a3246b
Jany Battung <janybattung@gmail.com>
2020-10-21 11:32:55 -0700

    deploying to netlify

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-
-import { Anchor } from 'antd';
 
 // const { LinkAnchor } = Anchor;
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener('resize', showButton);
 
   return (
     <>
