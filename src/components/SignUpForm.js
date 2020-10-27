3cf7eb16f7282af3e17376d85e81d782f13dd9b0
Jany Battung <janybattung@gmail.com>
2020-10-27 12:44:37 -0700

    update css

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -141,18 +141,6 @@ export default class SignUpForm extends Component {
         alert(`${this.state.firstName} Registered Successfully!`);
       })
       .catch((err) => console.log(err));
-    // this.setState({
-    //   firstName: "",
-    //   lastName: "",
-    //   email: "",
-    //   address1: "",
-    //   address2: "",
-    //   city: "",
-    //   state: "",
-    //   phoneNumber: "",
-    //   website: "",
-    //   zipCode: "",
-    // });
   };
 
   render() {
@@ -160,152 +148,152 @@ export default class SignUpForm extends Component {
       <div className="form-container">
         <form onSubmit={this.handleSubmit}>
           <h1>REGISTER A CHARITABLE ORGANIZATION</h1>
-          <table>
-            <tr>
-              <td>
-                <label>Charity Name: </label>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={this.state.charityName}
-                  onChange={this.namehandler}
-                  placeholder="charity name"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>First Name: </label>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={this.state.firstName}
-                  onChange={this.firsthandler}
-                  placeholder="Jane"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Last Name: </label>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={this.state.lastName}
-                  onChange={this.lasthandler}
-                  placeholder="Smith"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Email: </label>
-              </td>
-              <td>
-                <input
-                  type="email"
-                  value={this.state.email}
-                  onChange={this.emailhandler}
-                  placeholder="email@email.com"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Address1: </label>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={this.state.address1}
-                  onChange={this.address1handler}
-                  placeholder="address1"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Address2: </label>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={this.state.address2}
-                  onChange={this.address2handler}
-                  placeholder="address2"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>City: </label>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={this.state.city}
-                  onChange={this.cityhandler}
-                  placeholder="city"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>State: </label>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={this.state.state}
-                  onChange={this.statehandler}
-                  placeholder="state"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Zip Code: </label>
-              </td>
-              <td>
-                <input
-                  type="number"
-                  value={this.state.zipCode}
-                  onChange={this.ziphandler}
-                  placeholder="zip code"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Phone Number: </label>
-              </td>
-              <td>
-                <input
-                  type="tel"
-                  value={this.state.phoneNumber}
-                  onChange={this.phonehandler}
-                  placeholder="1234567890"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Website: </label>
-              </td>
-              <td>
-                <input
-                  type="url"
-                  value={this.state.website}
-                  onChange={this.websitehandler}
-                  placeholder="http://hello.com"/>
-              </td>
-            </tr>
-          </table>
-          {this.state.error && <div>{this.state.error}</div>}
-          <input type="submit" value="Register" />
+            <div className="form-label-input">
+              <div className="row">
+                <div className="form-label">
+                    <label>Charity Name: </label>
+                </div>    
+                <div className="form-input">
+                    <input
+                      type="text"
+                      value={this.state.charityName}
+                      onChange={this.namehandler}
+                      placeholder="charity name"
+                    />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                    <label>First Name: </label>
+                </div>
+                <div className="form-input">
+                    <input
+                      type="text"
+                      value={this.state.firstName}
+                      onChange={this.firsthandler}
+                      placeholder="Jane"
+                    />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                    <label>Last Name: </label>
+                </div>
+                <div className="form-input">
+                    <input
+                      type="text"
+                      value={this.state.lastName}
+                      onChange={this.lasthandler}
+                      placeholder="Smith"
+                    />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                    <label>Email: </label>
+                </div>
+                <div className="form-input">
+                    <input
+                      type="email"
+                      value={this.state.email}
+                      onChange={this.emailhandler}
+                      placeholder="email@email.com"
+                    />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                      <label>Address1: </label>
+                </div>
+                <div className="form-input">
+                      <input
+                        type="text"
+                        value={this.state.address1}
+                        onChange={this.address1handler}
+                        placeholder="address1"
+                      />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                      <label>Address2: </label>
+                </div>
+                <div className="form-input">
+                      <input
+                        type="text"
+                        value={this.state.address2}
+                        onChange={this.address2handler}
+                        placeholder="address2"
+                      />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                      <label>City: </label>
+                </div>
+                <div className="form-input">
+                      <input
+                        type="text"
+                        value={this.state.city}
+                        onChange={this.cityhandler}
+                        placeholder="city"
+                      />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                      <label>State: </label>
+                </div>
+                <div className="form-input">
+                      <input
+                        type="text"
+                        value={this.state.state}
+                        onChange={this.statehandler}
+                        placeholder="state"
+                      />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                      <label>Zip Code: </label>
+                </div>
+                <div className="form-input">
+                      <input
+                        type="number"
+                        value={this.state.zipCode}
+                        onChange={this.ziphandler}
+                        placeholder="zip code"
+                      />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-label">
+                      <label>Phone Number: </label>
+                </div>
+                <div className="form-input">
+                      <input
+                        type="tel"
+                        value={this.state.phoneNumber}
+                        onChange={this.phonehandler}
+                        placeholder="1234567890"
+                      />
+                </div>     
+              </div>
+              <div className="row">  
+                <div className="form-label">
+                      <label>Website: </label>
+                </div>
+                <div className="form-input">
+                      <input
+                        type="url"
+                        value={this.state.website}
+                        onChange={this.websitehandler}
+                        placeholder="http://hello.com"/>
+                </div>
+              </div>
+                  {this.state.error && <div>{this.state.error}</div>}
+                  <input type="submit" value="Register" />
+            </div>
         </form>
       </div>
     );
--- src/components/SignUpForm.test.js
+++ src/components/SignUpForm.test.js
@@ -1,28 +1,10 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
-import SignUpForm from './SignUpForm'
+import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+import SignUpForm from './SignUpForm';
 
-configure({ adapter: new Adapter() });
-
-describe(`SignUpForm component`, () => {
-  const props = {
-    className: 'test-class-name',
-    children: <p>test children</p>,
-    'data-other': 'test-other-prop'
-  }
-
-  it('renders a form.SignUpForm by default', () => {
-    const wrapper = shallow(<SignUpForm />)
-    expect(toJson(wrapper)).toMatchSnapshot()
-  })
-
-  it('renders the SignUpForm given props', () => {
-    const wrapper = shallow(<SignUpForm {...props} />)
-    expect(toJson(wrapper)).toMatchSnapshot()
-  })
-
-})
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<MemoryRouter><SignUpForm /></MemoryRouter>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
