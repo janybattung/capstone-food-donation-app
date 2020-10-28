920e717fd377cccc2e5e13f5b31e3450a99000c2
Jany Battung <janybattung@gmail.com>
2020-10-28 16:17:48 -0700

    update css

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -5,13 +5,15 @@ import './ContactSection.css';
 function ContactSection() {
   return (
     <div id="contact" className='contact-container'>
-      <div>
+      <div className="box-img">
         <img className='img-contact' src="/images/contact-us.jpg" alt="Contact Us"/>  
       </div>
-      <div className='contact-right'>
-        <h1>Contact</h1> 
-        <p>Email: info@beathunger.com</p><br />
-        <p>Phone: 1-800-245-0000</p>
+      <div className='box-text'>
+        <div className="text">
+          <h1>Contact</h1> 
+          <p>Email: info@beathunger.com</p><br />
+          <p>Phone: 1-800-245-0000</p>
+        </div>
       </div>    
     </div>
     
