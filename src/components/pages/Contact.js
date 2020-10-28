15272837eb8a122cc1fbde4ca09477543e2acdc2
Jany Battung <janybattung@gmail.com>
2020-10-28 16:34:45 -0700

    update css

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,12 @@
 import React from 'react';
 // import Home from '../pages/Home';
 import ContactSection from '../ContactSection';
-import Footer from '../Footer';
+
 
 function Contact() {
     return (
         <div>
            <ContactSection />
-           <Footer />
         </div>
     )
 }
