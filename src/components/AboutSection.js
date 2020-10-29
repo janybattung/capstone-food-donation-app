bfe17636492c3c8d3c76f91b4e76e55185dd42db
Jany Battung <janybattung@gmail.com>
2020-10-29 12:58:47 -0700

    update css

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,7 +7,7 @@ function AboutSection() {
     <div className='about-container'>
       <div className="charity-content">
         <h1>Who We Are</h1>
-        <p className="text">There are a lot of people all over the world barely affording a meal every day. Since the world population is growing, there are more and more mouths to feed, and we just can’t afford wasting any resources. The best thing that we can do is to immediately avoid situations where we throw food away and instead we donate it. That’s where BEAT HUNGER comes into play.
+        <p>There are a lot of people all over the world barely affording a meal every day. Since the world population is growing, there are more and more mouths to feed, and we just can’t afford wasting any resources. The best thing that we can do is to immediately avoid situations where we throw food away and instead we donate it. That’s where BEAT HUNGER comes into play.
         Our focus with BEAT HUNGER is to help those people in dire need of help gain access to all the food they need. We are heavily committed to quality and value, and we always guarantee that you will feel great knowing the food you donated actually goes to people that really need it. <br /><br />
         Charities are always looking for food that they can donate to people suffering from hunger. Thankfully, we can help you donate all the food you have no use for to charities from all over the country. Even the smallest donation will help people dealing with hunger, so don’t hesitate and avail this opportunity today!
         </p>
