0a9893c54c19005b100315816a41b5b937b0408e
Jany Battung <janybattung@gmail.com>
2020-10-29 15:47:23 -0700

    update files

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
-// import Button from './Button';
 import Charities from './pages/Charities';
 
 it('renders without crashing', () => {
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,81 +1,11 @@
 import React from 'react';
 import './Footer.css';
-// import { Button } from './Button';
-// import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div className='footer-container'>
-    {/* ---- Subscribe */}
-      {/* <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Subscribe to our newsletter
-        </p>
-        
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section> */}
-      {/* ----Social Media */}
-    
       <small className='website-rights'>BEAT HUNGER © 2020</small>
-
-      {/* <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='social-icons'>
-            <Link
-              className='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i className='fab fa-facebook-f' />
-            </Link>
-            <Link
-              className='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i className='fab fa-instagram' />
-            </Link>
-            <Link
-              className='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i className='fab fa-youtube' />
-            </Link>
-            <Link
-              className='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i className='fab fa-twitter' />
-            </Link>
-            <Link
-              className='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i className='fab fa-linkedin' />
-            </Link>
-          </div> */}
-        {/* </div>
-      </section> */}
     </div>
   );
 }
-
 export default Footer;
